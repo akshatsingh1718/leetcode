074da3a1d39c4dcc8058bef237eaecbb4f8b4145
Akshat Singh <akshat.singh1718@gmail.com>
2024-06-16 19:57:12 +0530

    16-june-24

---
+++ patchingArray/solution.py
@@ -0,0 +1,153 @@
+from typing import List, Optional, Union, Dict, Tuple, Set
+from bisect import bisect, bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from functools import cache
+from math import floor, ceil
+from heapq import heapify, heappop, heappush
+
+import sys
+
+# Check the current recursion limit
+current_limit = sys.getrecursionlimit()
+
+# Set a new recursion limit
+new_limit = 10**5  # Set this to the desired limit
+sys.setrecursionlimit(new_limit)
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def isListsSame(list1: ListNode, list2: ListNode):
+    head1, head2 = list1, list2
+    while (head1 != None) or (head2 != None):
+        if head1.val != head2.val:
+            return False
+        head1 = head1.next
+        head2 = head2.next
+
+    return True
+
+
+def printList(head: ListNode):
+    lstr: str = ""
+    while head is not None:
+        lstr += str(head.val) + ", "
+        head = head.next
+
+    print(lstr)
+
+
+# Graph utils
+def create_adjacency_list(edges: List[tuple]):
+    adjacency_list = {}
+
+    for edge in edges:
+        u, v = edge
+        if u not in adjacency_list:
+            adjacency_list[u] = []
+        if v not in adjacency_list:
+            adjacency_list[v] = []
+        adjacency_list[u].append(v)
+        adjacency_list[v].append(u)
+
+    return adjacency_list
+
+
+# List utils
+
+
+def list_to_ll(vector: List[int]):
+    prv_node = None
+    for i, val in enumerate(vector[::-1]):
+        prv_node = ListNode(val, prv_node)
+    return prv_node
+
+
+# Tree Utils
+def list_to_binary_tree(lst: List[int]):
+    if not lst:
+        return None
+
+    root = TreeNode(lst[0])
+    queue = [root]
+    i = 1
+
+    while queue and i < len(lst):
+        current_node = queue.pop(0)
+
+        if lst[i] is not None:
+            current_node.left = TreeNode(lst[i])
+            queue.append(current_node.left)
+
+        i += 1
+
+        if i < len(lst) and lst[i] is not None:
+            current_node.right = TreeNode(lst[i])
+            queue.append(current_node.right)
+
+        i += 1
+
+    return root
+
+
+###################################################
+################# Code Goes Here ##################
+###################################################
+"""
+Problem:
+Help:
+"""
+
+
+class Solution:
+    """
+    ==========================
+    Time and space complexity:
+    ==========================
+    TC:
+    SC:
+
+    ==========================
+    Algorithm:
+    ==========================
+    """
+
+    def minPatches(self, nums: List[int], n: int) -> int:
+        running_sum = []
+
+        i = 0
+        patches = 0
+        running_sum = 0
+
+        while running_sum < n:
+            if i < len(nums) and nums[i] <= running_sum + 1:
+                running_sum += nums[i]
+                i += 1
+            else:
+                patches += 1
+                running_sum += (running_sum + 1)
+
+        return patches
+
+
+def main():
+    obj = Solution()
+    nums = [1, 3]
+    n = 6
+    output = 1
+    print(obj.minPatches(nums, n))
+
+
+if __name__ == "__main__":
+    main()
